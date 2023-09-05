990ac09a31e661f1a20d51c4694c88abb83c51f5
rion suzuki <rion2002m@gmail.com>
2023-09-05 18:25:23 +0900

    敵マネージャー作業途中

--- application/Sourcecode/Enemy/EnemyManager.cpp
+++ application/Sourcecode/Enemy/EnemyManager.cpp
@@ -1,1 +1,19 @@
 #include "EnemyManager.h"
+#pragma region
+EnemyManager::EnemyManager()
+{
+}
+#pragma endregion
+
+#pragma region
+EnemyPopDataList::EnemyPopDataList()
+{
+	EnemyPopData stage1;
+	stage1.enemyData.resize(3);
+	//stage1.enemyData.at(0)
+
+}
+
+
+#pragma endregion
+
--- application/Sourcecode/Enemy/EnemyManager.h
+++ application/Sourcecode/Enemy/EnemyManager.h
@@ -1,13 +1,24 @@
 #pragma once
 #include "IEnemy.h"
 #include <vector>
+#include <unordered_map>
 class EnemyManager
 {
 private:
 	//“G
 	std::vector<std::unique_ptr<IEnemy>> enemyList_;
 	
-private:
+public:
 	EnemyManager();
 };
 
+struct EnemyPopData{
+	std::vector<IEnemy> enemyData;
+};
+
+class EnemyPopDataList {
+private:
+	std::unordered_map<std::string, EnemyPopData> dataList_;
+public:
+	EnemyPopDataList();
+};
